b08fb26707a3c001a1e9fdfeaa8b825847fa83b8
developer-rni <developer.rni@gmail.com>
2019-12-20 18:24:56 +0300

    top_menu and scroll animation

---
+++ #source/js/script.js
@@ -0,0 +1,10 @@
+var margin = 100; // переменная для контроля докрутки
+   $("a.scroll, a.glitch-btn").click(function() { // тут пишите условия, для всех ссылок или для конкретных
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top // .top+margin - ставьте минус, если хотите увеличить отступ
+      }, {
+         duration: 1600, // тут можно контролировать скорость
+         easing: "swing"
+      });
+      return false;
+   });
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 //@prepros-append jq-start.js
 //@prepros-append script.js
 //@prepros-append jq-end.js
+$(document).ready(function(){
+
+var margin = 100; // переменная для контроля докрутки
+   $("a.scroll, a.glitch-btn").click(function() { // тут пишите условия, для всех ссылок или для конкретных
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top // .top+margin - ставьте минус, если хотите увеличить отступ
+      }, {
+         duration: 1600, // тут можно контролировать скорость
+         easing: "swing"
+      });
+      return false;
+   });
+});
